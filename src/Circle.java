36cc2ea730c17b6f303e98417ec8a520e68570c4
User <madilovamairamkan@gmail.com>
2023-01-20 17:57:28 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,23 @@
+public class Circle {
+    private int PI;
+
+    static void area(){
+
+    }
+
+    public Circle(int PI) {
+        this.PI = PI;
+    }
+
+    static void circumferens(){
+
+    }
+
+    public int getPI() {
+        return PI;
+    }
+
+    public void setPI(int PI) {
+        this.PI = PI;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        double radius = 15.337;
+        double circumference = (2*pi*radius);
+        double area = (pi*(radius*2));
+        System.out.println( radius);
+                System.out.print( area);
+
+    }
+
+}
+
